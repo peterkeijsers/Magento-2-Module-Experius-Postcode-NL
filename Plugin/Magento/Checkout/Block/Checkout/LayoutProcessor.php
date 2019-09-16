1e1d870000e6806cd855c23b55a9acad475032fe
Peter Keijsers <peter.keijsers@experius.nl>
2019-09-16 17:27:27 +0000

    [REFACTOR] Refactored layoutprocessor to a afterProcess plugin
    This due to a possible order/execution conflict where in some rare cases the postcode was not being set properly on the quote billing address

--- Plugin/Magento/Checkout/Block/Checkout/LayoutProcessor.php
+++ Plugin/Magento/Checkout/Block/Checkout/LayoutProcessor.php
@@ -1,30 +1,8 @@
 <?php
-/**
- * A Magento 2 module named Experius Postcode
- * Copyright (C) 2017 Experius
- *
- * This file is part of Experius Postcode.
- *
- * Experius Postcode is free software: you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation, either version 3 of the License, or
- * (at your option) any later version.
- *
- * This program is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
- * GNU General Public License for more details.
- *
- * You should have received a copy of the GNU General Public License
- * along with this program. If not, see <http://www.gnu.org/licenses/>.
- */
 
-namespace Experius\Postcode\Block\Checkout;
+namespace Experius\Postcode\Plugin\Magento\Checkout\Block\Checkout;
 
-use Magento\Framework\View\Element\AbstractBlock;
-use Magento\Checkout\Block\Checkout\LayoutProcessorInterface;
-
-class LayoutProcessor extends AbstractBlock implements LayoutProcessorInterface
+class LayoutProcessor
 {
 
     protected $scopeConfig;
@@ -36,21 +14,23 @@ class LayoutProcessor extends AbstractBlock implements LayoutProcessorInterface
     public function __construct(
         \Magento\Framework\View\Element\Template\Context $context,
         array $data = []
-    ) {
+    )
+    {
         $this->scopeConfig = $context->getScopeConfig();
         $this->logger = $context->getLogger();
-
-        parent::__construct($context, $data);
     }
 
-    public function process($result)
+    public function afterProcess(
+        \Magento\Checkout\Block\Checkout\LayoutProcessor $subject,
+        $result
+    )
     {
         if ($this->scopeConfig->getValue(
-            'postcodenl_api/general/enabled',
-            \Magento\Store\Model\ScopeInterface::SCOPE_STORE
-        )
-        && isset($result['components']['checkout']['children']['steps']['children']['shipping-step']['children']
-            ['shippingAddress']['children']['shipping-address-fieldset'])
+                'postcodenl_api/general/enabled',
+                \Magento\Store\Model\ScopeInterface::SCOPE_STORE
+            )
+            && isset($result['components']['checkout']['children']['steps']['children']['shipping-step']['children']
+                ['shippingAddress']['children']['shipping-address-fieldset'])
         ) {
             $this->changeFieldPositions = $this->scopeConfig->getValue(
                 'postcodenl_api/advanced_config/change_sort_order',
@@ -80,6 +60,7 @@ class LayoutProcessor extends AbstractBlock implements LayoutProcessorInterface
 
             $result = $this->getBillingFormFields($result);
         }
+        
         return $result;
     }
 
@@ -134,8 +115,7 @@ class LayoutProcessor extends AbstractBlock implements LayoutProcessorInterface
                     'config' => [
                         "customScope" => $scope,
                         "template" => 'Experius_Postcode/form/group',
-                        "additionalClasses" => "experius_postcode_fieldset",
-                        "loaderImageHref" => $this->getViewFileUrl('images/loader-1.gif')
+                        "additionalClasses" => "experius_postcode_fieldset"
                     ],
                     'sortOrder' => '905',
                     'children' => $this->getPostcodeFields($scope, $addressType),
